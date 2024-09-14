2e69045996a4d9e606160169cab4d8724e9b24be
Matias La Mota <71835528+mlamota2020@users.noreply.github.com>
2024-09-14 11:48:25 -0500

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import datetime
 import time
-import pyfirmata2
+import pyfirmata
 import pygame
 import pyaudio
 
@@ -26,17 +26,27 @@ output_stream = p.open(
 
 print('detected\nreading and writing microphone...')
 
-t_h = 19
-t_m = 58
+t_h = 11
+t_m = 32
 t_s = 00
-pygame.mixer.music.load("C:\\Users\\inval\\OneDrive\\Documentos\\Audacity\\BEAM - PDF.wav") # Reemplazar
+pygame.mixer.music.load("C:\\Users\\Robotica1\\Downloads\\03 No soy Eterno.flac") # Reemplazar
 
-board = pyfirmata2.Arduino('COM3')
+board = pyfirmata.Arduino('COM7')
+
+LCD_PRINT = 0x01
+LCD_CLEAR = 0x02
+LCD_SET_CURSOR = 0x03
 
 while True:
     now = datetime.datetime.now()
     frmttime = now.strftime('%H:%M:%S')
-    print(f"It's {frmttime}.")
+    print(frmttime)
+
+    message = frmttime
+    board.send_sysex(LCD_CLEAR, [])
+    message_bytes = [ord(char) for char in message]
+    board.send_sysex(LCD_SET_CURSOR, [0, 0])
+    board.send_sysex(LCD_PRINT, message_bytes)
 
     if (now.hour == t_h and now.minute == t_m and now.second == t_s):
         print('¡Hora del almuerzo para Bachillerato! (14:58 PM) \n\n Preparando alarma...')
@@ -44,11 +54,10 @@ while True:
         
         for x in range(0, 4):
             board.digital[13].write(1)
-            print('1')
             time.sleep(0.100)
             board.digital[13].write(0)
-            print('0')
             time.sleep(0.100)
 
 
     time.sleep(1)
+    board.send_sysex(LCD_CLEAR, [])
